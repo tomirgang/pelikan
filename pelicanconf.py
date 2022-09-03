a870e64c6c8ffaefc11a240e649e1de00a451030
Tom <thomas@irgang.eu>
2022-09-03 12:29:18 +0200

    layout fine tuning

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,6 @@
 AUTHOR = 'Tom'
 SITENAME = "Tom's Blog"
-SITEURL = ''
+SITEURL = 'https://tomirgang.de'
 
 PATH = 'content'
 
@@ -33,6 +33,28 @@ DEFAULT_PAGINATION = 10
 # Enalbe theme
 THEME = 'blue-penguin-dark'
 
+FAVICON = 'images/logo.png'
+
 # Search settings
 SEARCH_MODE = "output"
 SEARCH_HTML_SELECTOR = "article"
+
+# Theme settings
+
+# Site Logo
+SITELOGO = 'images/logo.png'
+
+# HTML metadata
+SITEDESCRIPTION = 'Toms Blog'
+
+# provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
+CATEGORIES_URL     = 'categories'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_URL       = 'archives'
+ARCHIVES_SAVE_AS   = 'archives/index.html'
+
+# use those if you want pelican standard pages to appear in your menu
+MENU_INTERNAL_PAGES = (
+    ('Kategorien', CATEGORIES_URL, CATEGORIES_SAVE_AS),
+    ('Archiv', ARCHIVES_URL, ARCHIVES_SAVE_AS),
+)
